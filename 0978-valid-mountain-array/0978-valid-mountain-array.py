437ce16eb45ad2d2e49d631c4f40f457ff85fd97
Deepthi-0411 <deepthi.murali0411@gmail.com>
2025-07-16 22:44:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.py
+++ 0978-valid-mountain-array/0978-valid-mountain-array.py
@@ -1,16 +1,7 @@
-class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
-        n = len(arr)
-        if n < 3:
-            return False
-        i = 0
-
-        while i+1 < n and arr[i] < arr[i+1]:
-            i += 1
-        if i == 0 or i == n -1:
-            return False
-        while i+1 < n and arr[i] > arr[i+1]:
-            i += 1
-        return i == n-1
-
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
         
