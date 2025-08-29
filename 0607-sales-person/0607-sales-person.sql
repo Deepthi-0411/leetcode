f09f46ccedac016d9d6c25c24302783b2cc460c5
Deepthi-0411 <deepthi.murali0411@gmail.com>
2025-08-29 22:16:23 +0530

    Time: 1151 ms (99.49%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,1 +1,8 @@
-select s.name from salesperson s where not exists (select 1 from orders o join company c on c.com_id = o.com_id where o.sales_id = s.sales_id and c.name = 'RED');
+select s.name 
+from salesperson s 
+where not exists (
+    select 1 
+    from orders o 
+    join company c on c.com_id = o.com_id 
+    where o.sales_id = s.sales_id 
+    and c.name = 'RED');
